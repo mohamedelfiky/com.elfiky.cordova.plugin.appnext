5dba1dca794822e6efb4768c0245e2b7ef7106c6
mohamedelfiky <mohamed.elfikyy@yahoo.com>
2015-08-02 12:42:25 +0200

    js

--- js/AppnextPlugin.js
+++ js/AppnextPlugin.js
@@ -1,24 +1,23 @@
-
 var AppnextPlugin = {
-    
-    initializeWithPlacementID: function(placementID) {
-      return cordova.exec(null, null, 'Appnext', 'initializeWithPlacementID', [placementID]);
-    }
-  , showPopup: function() {
-      return cordova.exec(null, null, 'Appnext', 'showPopup', []);
-    }
-  , hidePopup: function() {
-      return cordova.exec(null, null, 'Appnext', 'hidePopup', []);
-    }
-  , cacheAd: function() {
-      return cordova.exec(null, null, 'Appnext', 'cacheAd', []);
-    }
-  , isVisible: function(successCallback) {
-      return cordova.exec(successCallback, null, 'Appnext', 'isVisible', []);
-    }
-  , setAdsCallback: function(successCallback, failureCallback) {
-      return cordova.exec(successCallback, failureCallback, 'Appnext', 'setAdsCallback', []);
-    }
+
+  initializeWithPlacementID: function (placementID) {
+    return cordova.exec(null, null, 'Appnext', 'initializeWithPlacementID', [placementID]);
+  }
+  , showPopup: function () {
+    return cordova.exec(null, null, 'Appnext', 'showPopup', []);
+  }
+  , hidePopup: function () {
+    return cordova.exec(null, null, 'Appnext', 'hidePopup', []);
+  }
+  , cacheAd: function () {
+    return cordova.exec(null, null, 'Appnext', 'cacheAd', []);
+  }
+  , isVisible: function (successCallback) {
+    return cordova.exec(successCallback, null, 'Appnext', 'isVisible', []);
+  }
+  , setAdsCallback: function (successCallback, failureCallback) {
+    return cordova.exec(successCallback, failureCallback, 'Appnext', 'setAdsCallback', []);
+  }
 };
 
-module.exports= AppnextPlugin;
+module.exports = AppnextPlugin;
